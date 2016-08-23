f61abc9c126a9262ddfd378908fc4e336138ee9e
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-08-23 16:29:08 +0300

    Code Formatting hw added!

---
+++ 02.High-Quality-Code-Part-1/01.CodeFormatting/FormattingCSharp/Event.cs
@@ -0,0 +1,63 @@
+﻿namespace FormattingCSharp
+{
+    using System;
+    using System.Text;
+
+    public class Event : IComparable
+    {
+        private DateTime date;
+        private string title;
+        private string location;
+
+        public Event(DateTime date, string title, string location)
+        {
+            this.Date = date;
+            this.Title = title;
+            this.Location = location;
+        }
+
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Location { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Event other = obj as Event;
+            int compareByDate = this.date.CompareTo(other.date);
+            int compareByTitle = this.title.CompareTo(other.title);
+            int compareByLocation = this.location.CompareTo(other.location);
+
+            if (compareByDate == 0)
+            {
+                if (compareByTitle == 0)
+                {
+                    return compareByLocation;
+                }
+                else
+                {
+                    return compareByTitle;
+                }
+            }
+            else
+            {
+                return compareByDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            var toString = new StringBuilder();
+            toString.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+            toString.Append(" | " + this.title);
+
+            if (this.location != null && this.location != string.Empty)
+            {
+                toString.Append(" | " + this.location);
+            }
+
+            return toString.ToString();
+        }
+    }
+}
---
+++ 02.High-Quality-Code-Part-1/01.CodeFormatting/FormattingCSharp/EventHolder.cs
@@ -0,0 +1,54 @@
+﻿namespace FormattingCSharp
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Event> title = new MultiDictionary<string, Event>(true);
+        private OrderedBag<Event> date = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+            this.title.Add(title.ToLower(), newEvent);
+            this.date.Add(newEvent);
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string deleteThisTitle = titleToDelete.ToLower();
+            int removed = 0;
+            foreach (var eventToRemove in this.title[deleteThisTitle])
+            {
+                removed++;
+                this.date.Remove(eventToRemove);
+            }
+
+            this.title.Remove(deleteThisTitle);
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.date.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+            int showed = 0;
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
