e83e4e3a7a365b3d7dae5bd160ad0884b25b5bc8
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-09-18 20:25:36 +0300

    Code tunning and optimization tasks added!

---
+++ 03.High-Quality-Code-Part-2/02.Code-Tuning-and-Optimization/CompareAdvancedMaths/CompareAdvancedMaths.cs
@@ -0,0 +1,67 @@
+﻿namespace CompareAdvancedMaths
+{
+    using System;
+    using System.Diagnostics;
+
+    public class CompareAdvancedMaths
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Results for Float");
+            float resultFloat = 0.0F;
+            ExecuteMathTests(resultFloat);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Double");
+            double resultDouble = 0.0;
+            ExecuteMathTests(resultDouble);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Decimal");
+            decimal resultDecimal = 0.0M;
+            ExecuteMathTests(resultDecimal);
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        private static void ExecuteMathTests(dynamic a)
+        {
+            const int LoopCount = 10000000;
+            a = a + 1;
+
+            Console.Write("Square Root: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    Math.Sqrt((double)a);
+                }
+            });
+
+            Console.Write("Natural algorithm: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    Math.Log((double)a);
+                }
+            });
+
+            Console.Write("Sinus: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    Math.Sin((double)a);
+                }
+            });
+        }
+    }
+}
---
+++ 03.High-Quality-Code-Part-2/02.Code-Tuning-and-Optimization/CompareSimpleMaths/CompareSimpleMaths.cs
@@ -0,0 +1,99 @@
+﻿namespace CompareSimpleMaths
+{
+    using System;
+    using System.Diagnostics;
+
+    public class CompareSimpleMaths
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Results for Integer");
+            int resultInt = 0;
+            ExecuteMathTests(resultInt);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Long");
+            long resultLong = 0;
+            ExecuteMathTests(resultLong);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Float");
+            float resultFloat = 0.0F;
+            ExecuteMathTests(resultFloat);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Double");
+            double resultDouble = 0.0;
+            ExecuteMathTests(resultDouble);
+            Console.WriteLine();
+
+            Console.WriteLine("Results for Decimal");
+            decimal resultDecimal = 0.0M;
+            ExecuteMathTests(resultDecimal);
+        }
+        
+        private static void DisplayExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        private static void ExecuteMathTests(dynamic a)
+        {
+            const int LoopCount = 10000000;
+
+            Console.Write("Add: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 0;
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    a = a + 1;
+                }
+            });
+
+            Console.Write("Subtract: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 10000000;
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    a = a - 1;
+                }
+            });
+
+            Console.Write("Increment: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 0;
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    a++;
+                }
+            });
+
+            Console.Write("Multiply: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 1;
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    a = a * 1;
+                }
+            });
+
+            Console.Write("Divide: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 1;
+                for (var i = 0; i < LoopCount; i++)
+                {
+                    a = a / 1;
+                }
+            });
+        }
+    }
+}
