a99a124c9f3261585058143381853c8f0be14c24
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-09-27 15:19:40 +0300

    Development tools tasks added!

---
+++ 03.High-Quality-Code-Part-2/04.Development-Tools/Log4NetExample/Log4Net.cs
@@ -0,0 +1,24 @@
+﻿namespace Log4NetExample
+{
+    using System;
+
+    using log4net;
+    using log4net.Config;
+
+    public class Log4Net
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4Net));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Debug("Starting application");
+            for (int i = 0; i < 15; i++)
+            {
+                Log.InfoFormat(DateTime.Now.ToString("yyyy.MM.dd-hh.mm.ss~fff"));
+            }
+
+            Log.Debug("Ending application");
+        }
+    }
+}
